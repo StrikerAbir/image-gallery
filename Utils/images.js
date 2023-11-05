e9c372e7dee7aec00e37ae069ad4d14f954e8aec
Abir Hasan <cse.170201063@gmail.com>
2023-11-05 14:14:22 +0600

    sortable functional done. initially

---
+++ Utils/images.js
@@ -0,0 +1,48 @@
+ const images = [
+  {
+    id: 1,
+   src:"/public/images/image-1.webp"
+  },
+  {
+    id: 2,
+   src:"/public/images/image-2.webp"
+  },
+  {
+    id: 3,
+   src:"/public/images/image-3.webp"
+  },
+  {
+    id: 4,
+   src:"/public/images/image-4.webp"
+  },
+  {
+    id: 5,
+   src:"/public/images/image-5.webp"
+  },
+  {
+    id: 6,
+   src:"/public/images/image-6.webp"
+  },
+  {
+    id: 7,
+   src:"/public/images/image-7.webp"
+  },
+  {
+    id: 8,
+   src:"/public/images/image-8.webp"
+  },
+  {
+    id: 9,
+   src:"/public/images/image-9.webp"
+  },
+  {
+    id: 10,
+   src:"/public/images/image-10.webp"
+  },
+  {
+    id: 11,
+   src:"/public/images/image-11.webp"
+  },
+];
+
+export default images
